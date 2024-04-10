bf682c50d9b0e7f1f3b442753e26b4d9d86d88f9
Dk2698 <dileepkumar1823@gmail.com>
2024-04-10 21:52:01 +0530

    setup-project

---
+++ src/main/java/com/kumar/example/ExampleApiApplication.java
@@ -0,0 +1,13 @@
+package com.kumar.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApiApplication.class, args);
+	}
+
+}
